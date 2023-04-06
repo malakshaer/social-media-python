f97cc0f72dd9a98095ff7186e5abb3e1b1131802
malakshaer <shaermk06@gmail.com>
2023-04-06 11:08:05 +0300

    backend: create flask app and connect the routes

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from datetime import timedelta
+from authAPI import auth
+from userAPI import user
+
+app = Flask(__name__)
+jwt = JWTManager(app)
+CORS(app)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/social-media"
+mongo = PyMongo(app)
+
+app.secret_key = 'secret key'
+app.config['JWT_SECRET_KEY'] = 'this-is-the-secret-key'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=120)
+
+
+app.register_blueprint(auth)
+app.register_blueprint(user)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
