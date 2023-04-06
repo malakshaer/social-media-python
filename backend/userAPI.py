4f50fe4396a474da8482fadafba4aca7281a2d9e
malakshaer <shaermk06@gmail.com>
2023-04-06 16:37:14 +0300

    backend: create follow function

--- backend/userAPI.py
+++ backend/userAPI.py
@@ -61,3 +61,65 @@ def make_account_private():
         return jsonify({'message': 'Account is now public.'}), 200
     else:
         return jsonify({'message': 'Account is now private.'}), 200
+
+
+@user.route('/follow/<user_id>', methods=['POST'])
+@jwt_required()
+def follow_user(user_id):
+    # Get the current user's ID and information
+    current_user_id = get_jwt_identity()
+    current_user = mongo.db.users.find_one({'_id': ObjectId(current_user_id)})
+    current_user_name = current_user['firstName'] + \
+        ' ' + current_user['lastName']
+
+    # Get the user being followed's information
+    user_to_follow = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+    user_to_follow_name = user_to_follow['firstName'] + \
+        ' ' + user_to_follow['lastName']
+    is_private = user_to_follow.get('is_private', False)
+
+    # Check if the current user is already following the other user
+    if user_id in current_user['following']:
+        return jsonify({'message': 'You are already following this user.'}), 400
+
+    # Check if the current user has already sent a follow request to the other user
+    if any(request['id'] == user_id for request in current_user.get('requested', [])):
+        return jsonify({'message': 'Follow request already sent.'}), 400
+
+    # If the user being followed is private, send a follow request
+    if is_private:
+        notification = {
+            'message': f'{current_user_name} wants to follow you.',
+            'sender': current_user_id,
+            'receiver': user_id,
+            'type': 'follow_request'
+        }
+        mongo.db.notifications.insert_one(notification)
+
+        # Create a requested list in the current user document and add the user ID and name of the user they requested to follow
+        mongo.db.users.update_one(
+            {'_id': ObjectId(current_user_id)},
+            {'$push': {'requested': {'id': user_id, 'name': user_to_follow_name}}}
+        )
+
+        # Create a request list in the other user document and add the user ID and name of the user who requested to follow
+        mongo.db.users.update_one(
+            {'_id': ObjectId(user_id)},
+            {'$push': {'request_list': {'id': current_user_id, 'name': current_user_name}}}
+        )
+
+        return jsonify({'message': 'Follow request sent.'}), 200
+
+    # Add the current user to the other user's followers list
+    mongo.db.users.update_one(
+        {'_id': ObjectId(user_id)},
+        {'$push': {'followers': {'id': current_user_id, 'name': current_user_name}}}
+    )
+
+    # Add the other user to the current user's following list
+    mongo.db.users.update_one(
+        {'_id': ObjectId(current_user_id)},
+        {'$push': {'following': {'id': user_id, 'name': user_to_follow_name}}}
+    )
+
+    return jsonify({'message': 'User followed successfully.'}), 200
