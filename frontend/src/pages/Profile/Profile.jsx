65eda7af7fafb599f20f4253de9e373b21a8d704
malakshaer <shaermk06@gmail.com>
2023-04-07 09:00:30 +0300

    frontend: add the users navigation bar component

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import profileImage from "../../assets/profileImage.png";
+import UsersNavigation from "../../components/UsersNavigation/UsersNavigation";
+import "./profile.css";
+import FollowersList from "../../components/FollowersList/FollowersList";
 
 const Profile = () => {
   return (
     <div>
       <Navbar />
-      <p>Profile</p>
+      <div>
+        <div className="profile-face">
+          <img src={profileImage} alt="" />
+          <p>Malak Shaer</p>
+        </div>
+        <div>
+          <UsersNavigation />
+          <FollowersList />
+        </div>
+      </div>
     </div>
   );
 };
