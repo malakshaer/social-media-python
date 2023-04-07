64218ab0edb959d0bb51b0e760e24c044f2d7b09
malakshaer <shaermk06@gmail.com>
2023-04-07 11:35:00 +0300

    frontend: render the components

--- frontend/src/components/UsersNavigation/UsersNavigation.jsx
+++ frontend/src/components/UsersNavigation/UsersNavigation.jsx
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "./usersnavigation.css";
+import FollowersList from "../FollowersList/FollowersList";
+import FollowingList from "../FollowingList/FollowingList";
+import RequestsList from "../RequestsList/RequestsList";
+import RequestSentList from "../RequestSentList/RequestSentList";
 
 const UsersNavigation = () => {
+  const [listType, setListType] = useState("followers");
+  const [isActive, setIsActive] = useState(false);
+
+  const renderList = () => {
+    switch (listType) {
+      case "followers":
+        return <FollowersList />;
+      case "following":
+        return <FollowingList />;
+      case "requests":
+        return <RequestsList />;
+      case "sentRequests":
+        return <RequestSentList />;
+      default:
+        return null;
+    }
+  };
   return (
-    <nav className="users-navigation">
-      <div className="users-navigation-menu">
-        <ul>
-          <li>
-            <a href="/">Followers</a>
-          </li>
-          <li>
-            <a href="/">Following</a>
-          </li>
-          <li>
-            <a href="/">Request List</a>
-          </li>
-          <li>
-            <a href="/">Requested</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <nav className="users-navigation">
+        <div className="users-navigation-menu">
+          <ul>
+            <li>
+              <button
+                className={isActive ? "active" : ""}
+                onClick={() => {
+                  setIsActive(true);
+                  setListType("followers");
+                }}
+              >
+                Followers
+              </button>
+            </li>
+            <li>
+              <button
+                className={isActive ? "active" : ""}
+                onClick={() => {
+                  setIsActive(true);
+                  setListType("following");
+                }}
+              >
+                Following
+              </button>
+            </li>
+            <li>
+              <button
+                className={isActive ? "active" : ""}
+                onClick={() => {
+                  setIsActive(true);
+                  setListType("requests");
+                }}
+              >
+                Requests List
+              </button>
+            </li>
+            <li>
+              <button
+                className={isActive ? "active" : ""}
+                onClick={() => {
+                  setIsActive(true);
+                  setListType("sentRequests");
+                }}
+              >
+                Sent Requests
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {renderList()}
+    </div>
   );
 };
 
