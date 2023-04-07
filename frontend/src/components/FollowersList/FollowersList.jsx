b13dbb481dcd35b42f597d6894ff9fd927d74904
malakshaer <shaermk06@gmail.com>
2023-04-07 11:39:14 +0300

    frontend: add the button props

--- frontend/src/components/FollowersList/FollowersList.jsx
+++ frontend/src/components/FollowersList/FollowersList.jsx
@@ -4,9 +4,9 @@ import "./followerslist.css";
 
 const FollowersList = () => {
   return (
-    <div className="users-cart">
+    <div className="users-card">
       {Users.map((user) => (
-        <User key={user._id} data={user} />
+        <User key={user._id} data={user} textButton="Remove" />
       ))}
     </div>
   );
