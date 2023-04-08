5058e342f4e8537af3da56bd46f8b56d4f5aa6f5
malakshaer <shaermk06@gmail.com>
2023-04-08 18:37:32 +0300

    frontend: connect request list with backend

--- frontend/src/components/RequestsList/RequestsList.jsx
+++ frontend/src/components/RequestsList/RequestsList.jsx
@@ -1,12 +1,50 @@
-import { Users } from "../../users";
+import React, { useState, useEffect } from "react";
 import User from "../User/User";
 import "./requestslist.css";
+import axios from "axios";
 
 const RequestsList = () => {
+  const [requestsWaiting, setRequestsWaiting] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("/get-request-list");
+        console.log(response);
+        const requestList = response.data.requestList.map((request) => {
+          return {
+            id: request.id,
+            name: request.name,
+          };
+        });
+        setRequestsWaiting(requestList);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleAcceptUser = async (userId) => {
+    try {
+      await axios.post(`/accept/${userId}`);
+      setRequestsWaiting((prevUsers) =>
+        prevUsers.filter((user) => user.id !== userId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="users-card">
-      {Users.map((user) => (
-        <User key={user._id} data={user} textButton="accept" />
+      {requestsWaiting.map((user) => (
+        <User
+          key={user.id}
+          name={user.name}
+          textButton="accept"
+          onClick={() => handleAcceptUser(user.id)}
+        />
       ))}
     </div>
   );
