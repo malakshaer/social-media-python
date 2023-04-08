2dffe82f4f6715b9cefce9bea7191044a6d4a5c0
malakshaer <shaermk06@gmail.com>
2023-04-08 18:39:51 +0300

    frontend: connect with the backend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import UserCard from "../../components/UserCard/UserCard";
+import axios from "axios";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchCurrentUser() {
+      const response = await axios.get("get-user-info");
+      setCurrentUser(response.data.id);
+    }
+    fetchCurrentUser();
+
+    async function fetchUsers() {
+      const response = await axios.get("get-all-users");
+      setUsers(response.data);
+    }
+    fetchUsers();
+  }, []);
+
+  const filteredUsers = users.filter((user) => user._id !== currentUser);
+
   return (
     <div className="home">
       <Navbar />
+      <div className="users-card">
+        {filteredUsers.map((user) => (
+          <UserCard key={user._id} data={user} />
+        ))}
+      </div>
     </div>
   );
 };
