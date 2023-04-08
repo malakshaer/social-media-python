9c5e117e5a3d8ee3e5996025475b9ed4bea9a1aa
malakshaer <shaermk06@gmail.com>
2023-04-08 19:13:31 +0300

    frontend: add private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
@@ -6,7 +7,7 @@ import Home from "./pages/Home/Home";
 import Logout from "./components/Logout/Logout";
 import Profile from "./pages/Profile/Profile";
 import EditAccount from "./components/EditAccount/EditAccount";
-// import PrivateRoute from "./contexts/PrivateRoute";
+import PrivateRoute from "./contexts/PrivateRoute";
 
 function App() {
   return (
@@ -14,10 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/edit-account" element={<EditAccount />} />
+        <Route exact path="/" element={<PrivateRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/edit-account" element={<EditAccount />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
