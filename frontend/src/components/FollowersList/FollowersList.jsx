8557cf3891108279401040cc3dfee5798ff6c68e
malakshaer <shaermk06@gmail.com>
2023-04-08 18:45:22 +0300

    frontend: connect with the backend

--- frontend/src/components/FollowersList/FollowersList.jsx
+++ frontend/src/components/FollowersList/FollowersList.jsx
@@ -1,12 +1,49 @@
-import { Users } from "../../users";
+import React, { useState, useEffect } from "react";
 import User from "../User/User";
 import "./followerslist.css";
+import axios from "axios";
 
 const FollowersList = () => {
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("/get-followers");
+        const followersList = response.data.followers.map((follower) => {
+          return {
+            id: follower.id,
+            name: follower.name,
+          };
+        });
+        setFollowers(followersList);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleRemoveFollower = async (userId) => {
+    try {
+      await axios.post(`/remove-follower/${userId}`);
+      setFollowers((prevUser) =>
+        prevUser.filter((follower) => follower.id !== userId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="users-card">
-      {Users.map((user) => (
-        <User key={user._id} data={user} textButton="Remove" />
+      {followers.map((user) => (
+        <User
+          key={user.id}
+          name={user.name}
+          textButton="Remove"
+          onClick={() => handleRemoveFollower(user.id)}
+        />
       ))}
     </div>
   );
