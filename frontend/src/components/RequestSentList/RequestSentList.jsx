283501221ec99c3e444b9e839ac0ce1e5ffe1d10
malakshaer <shaermk06@gmail.com>
2023-04-08 18:38:05 +0300

    frontend: connect the request sent list with the backend

--- frontend/src/components/RequestSentList/RequestSentList.jsx
+++ frontend/src/components/RequestSentList/RequestSentList.jsx
@@ -1,12 +1,49 @@
-import { Users } from "../../users";
+import React, { useState, useEffect } from "react";
 import User from "../User/User";
 import "./requestsentlist.css";
+import axios from "axios";
 
 const RequestSendList = () => {
+  const [sentToUsers, setSentToUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("/get-sent-requests");
+        const sentRequestList = response.data.requested.map((user) => {
+          return {
+            id: user.id,
+            name: user.name,
+          };
+        });
+        setSentToUsers(sentRequestList);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleDeleteRequest = async (userId) => {
+    try {
+      await axios.delete(`/delete-request/${userId}`);
+      setSentToUsers((prevUsers) =>
+        prevUsers.filter((user) => user.id !== userId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="users-card">
-      {Users.map((user) => (
-        <User key={user._id} data={user} textButton="delete request" />
+      {sentToUsers.map((user) => (
+        <User
+          key={user.id}
+          name={user.name}
+          textButton="delete request"
+          onClick={() => handleDeleteRequest(user.id)}
+        />
       ))}
     </div>
   );
