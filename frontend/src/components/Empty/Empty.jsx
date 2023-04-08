6623b284d0683a0f641cb4ae2defa131a205a4e0
malakshaer <shaermk06@gmail.com>
2023-04-08 18:44:24 +0300

    frontend: create empty state component

---
+++ frontend/src/components/Empty/Empty.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./empty.css";
+
+const Empty = () => {
+  return (
+    <div className="empty-container">
+      <p>No users yet</p>
+    </div>
+  );
+};
+
+export default Empty;
