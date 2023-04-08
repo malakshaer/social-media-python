5d67271dcfbe4b7bf2c228d4711506d03f8e809b
malakshaer <shaermk06@gmail.com>
2023-04-08 18:42:43 +0300

    frontend:  connect with the backend

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
-import profileImage from "../../assets/profileImage.png";
 import "./profile.css";
 import UsersNavigation from "../../components/UsersNavigation/UsersNavigation";
+import Button from "../../components/Button/Button";
+import axios from "axios";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    numberFollowers: 0,
+    numberFollowing: 0,
+    bio: "",
+  });
+
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const headers = {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        };
+
+        const response = await axios.get("/get-user-info", { headers });
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getProfile();
+  }, []);
+
+  const handleEditAccount = () => {
+    navigate("/edit-account");
+  };
+
   return (
     <div>
       <Navbar />
-      <div>
-        <div className="profile-face">
-          <img src={profileImage} alt="" />
-          <p>Malak Shaer</p>
+      <div className="user-card">
+        <div className="user-card-left">
+          <p className="user-card-name">{`${user.firstName} ${user.lastName}`}</p>
+          <span className="user-card-bio">{user.bio}</span>
+          <p className="user-card-count">{`Followers: ${user.numberFollowers} | Following: ${user.numberFollowing}`}</p>
         </div>
-        <UsersNavigation />
+        <div className="user-card-right">
+          <Button text={"Edit Account"} onClick={handleEditAccount} />
+        </div>
       </div>
+      <UsersNavigation />
     </div>
   );
 };
