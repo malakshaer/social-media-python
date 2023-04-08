923efc04e5e74a7808a353b0c4688a73f095b15b
malakshaer <shaermk06@gmail.com>
2023-04-08 18:38:26 +0300

    frontend: connect the following list with the backend

--- frontend/src/components/FollowingList/FollowingList.jsx
+++ frontend/src/components/FollowingList/FollowingList.jsx
@@ -1,12 +1,49 @@
-import { Users } from "../../users";
+import React, { useState, useEffect } from "react";
 import User from "../User/User";
 import "./followinglist.css";
+import axios from "axios";
 
 const FollowingList = () => {
+  const [followings, setFollowings] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("/get-following");
+        const followingList = response.data.following.map((following) => {
+          return {
+            id: following.id,
+            name: following.name,
+          };
+        });
+        setFollowings(followingList);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleUnFollow = async (userId) => {
+    try {
+      await axios.put(`/unfollow/${userId}`);
+      setFollowings((prevFollowers) =>
+        prevFollowers.filter((follower) => follower.id !== userId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="users-card">
-      {Users.map((user) => (
-        <User key={user._id} data={user} textButton="unfollow" />
+      {followings.map((user) => (
+        <User
+          key={user.id}
+          name={user.name}
+          textButton="unfollow"
+          onClick={() => handleUnFollow(user.id)}
+        />
       ))}
     </div>
   );
